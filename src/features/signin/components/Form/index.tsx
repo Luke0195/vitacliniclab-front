ddeccd7b7218d9dead0d301c7f99946936eb3e21
Lucas Santos <lucas201273@gmail.com>
2023-12-26 10:12:32 -0300

    feat: add animation on signin page

--- src/features/signin/components/Form/index.tsx
+++ src/features/signin/components/Form/index.tsx
@@ -1,38 +1,61 @@
 import * as S from '../../ui/styles'
 import { ButtonRoot, InputRoot } from '@app/components/Form'
 import { FiMail, FiLock } from 'react-icons/fi'
+import { motion } from 'framer-motion'
 
 export function Form() {
   return (
     <S.FormComponent>
       <form>
         <h2> Faça seu Login</h2>
-        <InputRoot.InputWrapper>
-          <InputRoot.InputLabel content="E-mail" />
-          <InputRoot.InputField>
-            <InputRoot.InputIcon icon={<FiMail size={20} color="#c8c8c8" />} />
-            <InputRoot.Input
-              type="email"
-              name="email"
-              placeholder="Entre com o seu email"
-            />
-          </InputRoot.InputField>
-        </InputRoot.InputWrapper>
-        <InputRoot.InputWrapper>
-          <InputRoot.InputLabel content="Senha" />
-          <InputRoot.InputField>
-            <InputRoot.InputIcon icon={<FiLock size={20} color="#c8c8c8" />} />
-            <InputRoot.Input
-              type="email"
-              name="email"
-              placeholder="Entre com o seu email"
-            />
-          </InputRoot.InputField>
-        </InputRoot.InputWrapper>
+        <motion.div
+          initial={{ x: -30, opacity: 0.5 }}
+          style={{ width: '100%' }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2 }}>
+          <InputRoot.InputWrapper>
+            <InputRoot.InputLabel content="E-mail" />
+            <InputRoot.InputField>
+              <InputRoot.InputIcon
+                icon={<FiMail size={20} color="#c8c8c8" />}
+              />
+              <InputRoot.Input
+                type="email"
+                name="email"
+                placeholder="Entre com o seu email"
+              />
+            </InputRoot.InputField>
+          </InputRoot.InputWrapper>
+        </motion.div>
+        <motion.div
+          initial={{ x: 30, opacity: 0.3 }}
+          style={{ width: '100%' }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2 }}>
+          <InputRoot.InputWrapper>
+            <InputRoot.InputLabel content="Senha" />
+            <InputRoot.InputField>
+              <InputRoot.InputIcon
+                icon={<FiLock size={20} color="#c8c8c8" />}
+              />
+              <InputRoot.Input
+                type="email"
+                name="email"
+                placeholder="Entre com o seu email"
+              />
+            </InputRoot.InputField>
+          </InputRoot.InputWrapper>
+        </motion.div>
         <span> Esqueceu a senha ?</span>
-        <ButtonRoot.ButtonWrapper color="#a500ff" disabled={true}>
-          Entrar
-        </ButtonRoot.ButtonWrapper>
+        <motion.div
+          style={{ width: '100%' }}
+          initial={{ y: 30, opacity: 0.3 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 2 }}>
+          <ButtonRoot.ButtonWrapper color="#a500ff" disabled={true}>
+            Entrar
+          </ButtonRoot.ButtonWrapper>
+        </motion.div>
       </form>
     </S.FormComponent>
   )
--- src/features/signin/components/Info/index.tsx
+++ src/features/signin/components/Info/index.tsx
@@ -1,14 +1,25 @@
 import * as S from '../../ui/styles'
+import { motion } from 'framer-motion'
 
 export function Info() {
   return (
     <S.InfoComponent>
-      <p> O VitaLab é a melhor plataforma para clinicas e consultórios.</p>
-      <p>
-        {' '}
-        Gerencie o prontuário dos seus pacientes de forma simples e prática
-        através de uma plataforma intuitiva fácil de se usar.
-      </p>
+      <motion.div
+        initial={{ y: -30, opacity: 0.5 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}>
+        <p> O VitaLab é a melhor plataforma para clinicas e consultórios.</p>
+      </motion.div>
+      <motion.div
+        initial={{ x: -30, opacity: 0.5 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}>
+        <p>
+          {' '}
+          Gerencie o prontuário dos seus pacientes de forma simples e prática
+          através de uma plataforma intuitiva fácil de se usar.
+        </p>
+      </motion.div>
     </S.InfoComponent>
   )
 }
